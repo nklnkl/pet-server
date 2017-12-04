b8708fb50c672ba64d2e864b051f093591827819
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-04 16:21:10 -0500

    Update SessionRouter

---
+++ src/server/route/customer/session.ts
@@ -0,0 +1,53 @@
+import { SessionDb, AccountDb } from 'pet-db';
+import { Session, Account } from 'pet-entity';
+import { SessionService, AccountService } from 'pet-business';
+
+import { Router, Request, Response, NextFunction } from 'express';
+import { Connection } from 'mongoose';
+
+export { SessionRouter };
+
+class SessionRouter {
+    private router: Router;
+    private sessionDb: SessionDb;
+    private accountDb: AccountDb;
+
+    constructor (dbConnection: Connection) {
+      this.router = Router();
+      this.routes();
+      this.sessionDb = new SessionDb(dbConnection);
+      this.accountDb = new AccountDb(dbConnection);
+    }
+
+    public getRouter () : Router {
+      return this.router;
+    }
+
+    // Takes all methods and attaches them to end points.
+    private routes () : void {
+      this.router.post('/', this.create.bind(this));
+    }
+
+    private create (req: Request, res: Response, next: NextFunction) : void {
+			// Retrieve account owner's info.
+			if (!req.body.email) res.status(422).end();
+      if (!req.body.password) res.status(422).end();
+
+      let email: any = req.body.email;
+      let password: any = req.body.password;
+
+      this.accountDb.retrieveByEmail(email)
+      .then((account: Account) => AccountService.checkPassword(password, account.getPassword()))
+      .then((match: boolean) => this.accountDb.retrieveByEmail(email))
+      .then((account: Account) => SessionService.create(account.getId()))
+      .then((session: Session) => this.sessionDb.create(session))
+      .then((session: Session) => {
+        let object: Object = {
+          userId: session.getUserId(),
+          sessionId: session.getId()
+        };
+        res.status(200).json(object);
+      })
+      .catch((err: Error) => next(err));
+    }
+}
