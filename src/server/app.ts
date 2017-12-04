662e642cfc377f9e9ca543a7182c34676de47c18
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-04 16:45:18 -0500

    Add GuestRouter to application

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,22 +3,18 @@ import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
 
-// import { Client as DbClient } from '@cryptoproject/casino-db';
+import { Client as DbClient } from 'pet-db';
 
-// import { PlayerRouter } from './routes/player/router';
-// import { AdminRouter } from './routes/admin/router';
+import { GuestRouter } from './route/guest/router';
+import { CustomerRouter } from './route/customer/router';
+import { AdminRouter } from './route/admin/router';
 
 export { App };
 
 class App {
     private express: express.Application;
     private logging: boolean;
-    // private dbClient: DbClient;
-
-    /**
-    the database client that will come from the database module.
     private dbClient: DbClient;
-    **/
 
     constructor (logging: boolean) {
       this.logging = logging;
@@ -27,7 +23,6 @@ class App {
 
     public start () : Promise<any> {
       return new Promise ((resolve, reject) => {
-        /*
         this.dbClient = new DbClient();
         this.dbClient.connect()
         .then(() => {
@@ -37,43 +32,28 @@ class App {
           resolve();
         })
         .catch((err:any) => reject(err));
-        */
       });
     }
 
     private middleware () : void {
-        if (this.logging == true)
-            this.express.use(logger('dev'));
-        this.express.use(bodyParser.json());
-        this.express.use(bodyParser.urlencoded({ extended: false }));
+      if (this.logging == true)
+        this.express.use(logger('dev'));
+      this.express.use(bodyParser.json());
+      this.express.use(bodyParser.urlencoded({ extended: false }));
     }
 
     private routes () : void {
+      this.express.get('/', (req, res, next) => {
+          res.status(200).end();
+      });
+      let adminRouter: AdminRouter = new AdminRouter(this.dbClient.getConnection());
+      this.express.use('/admin', adminRouter.getRouter());
 
-        this.express.get('/', (req, res, next) => {
-            res.status(200).end();
-        });
-        /*
-        let adminRouter: AdminRouter = new AdminRouter(this.dbClient.getConnection());
-        this.express.use('/admin', adminRouter.getRouter());
+      let customerRouter: CustomerRouter = new CustomerRouter(this.dbClient.getConnection());
+      this.express.use('/customer', customerRouter.getRouter());
 
-        let playerRouter: PlayerRouter = new PlayerRouter(this.dbClient.getConnection());
-        this.express.use('/player', playerRouter.getRouter());
-        */
-        /*
-        these routers are commented out because there is no
-        database client to send to these routers.
-        for now, we are using routes with no connection to database above
-
-        let guestRouter: GuestRouter = new GuestRouter(this.dbClient);
-        this.express.use('/guest', guestRouter.getRouter());
-
-        let adminRouter: AdminRouter = new AdminRouter(this.dbClient);
-        this.express.use('/admin', adminRouter.getRouter());
-
-        let userRouter: UserRouter = new UserRouter(this.dbClient);
-        this.express.use('/user', userRouter.getRouter());
-        */
+      let guestRouter: GuestRouter = new GuestRouter(this.dbClient.getConnection());
+      this.express.use('/guest', guestRouter.getRouter());
     }
 
     private error () : void {
@@ -81,6 +61,6 @@ class App {
     }
 
     public getExpress () : express.Application {
-        return this.express;
+      return this.express;
     }
 }
